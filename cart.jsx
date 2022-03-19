49a49f94ce25ecea5d4d653f2dbd9ea4322961f4
Vera Phipps <85977298+veraphipps01@users.noreply.github.com>
2022-03-18 21:02:16 -0700

    Update cart.jsx

--- cart.jsx
+++ cart.jsx
@@ -1,6 +1,16 @@
+// simulate getting products from DataBase
+const products = [
+  { name: "Apples:", country: "Italy", cost: 3, instock: 10 },
+  { name: "Oranges:", country: "Spain", cost: 4, instock: 3 },
+  { name: "Beans:", country: "USA", cost: 2, instock: 5 },
+  { name: "Cabbage:", country: "Poland", cost: 1, instock: 8 },
+];
+//=========Cart=============
+const Cart = (props) => {
+  const { Card, Accordion, Button } = ReactBootstrap;
+  let data = props.location.data ? props.location.data : products;
+  console.log(`data:${JSON.stringify(data)}`);
 
-const Cart = () => {
-  const { Accordion } = ReactBootstrap;
   return <Accordion defaultActiveKey="0">{list}</Accordion>;
 };
 
@@ -13,15 +23,15 @@ const useDataApi = (initialUrl, initialData) => {
     isError: false,
     data: initialData,
   });
-
+  console.log(`useDataApi called`);
   useEffect(() => {
-
+    console.log("useEffect Called");
     let didCancel = false;
     const fetchData = async () => {
       dispatch({ type: "FETCH_INIT" });
       try {
         const result = await axios(url);
-
+        console.log("FETCH FROM URl");
         if (!didCancel) {
           dispatch({ type: "FETCH_SUCCESS", payload: result.data });
         }
@@ -38,7 +48,6 @@ const useDataApi = (initialUrl, initialData) => {
   }, [url]);
   return [state, setUrl];
 };
-
 const dataFetchReducer = (state, action) => {
   switch (action.type) {
     case "FETCH_INIT":
@@ -65,9 +74,10 @@ const dataFetchReducer = (state, action) => {
   }
 };
 
-const Products = () => {
-  const [items, setItems] = React.useState([]);
+const Products = (props) => {
+  const [items, setItems] = React.useState(products);
   const [cart, setCart] = React.useState([]);
+  const [total, setTotal] = React.useState(0);
   const {
     Card,
     Accordion,
@@ -76,87 +86,72 @@ const Products = () => {
     Row,
     Col,
     Image,
+    Input,
   } = ReactBootstrap;
-
   //  Fetch Data
-  const { useState, useEffect } = React;
-  const [query, setQuery] = useState("http://localhost:1337/api/products");
+  const { Fragment, useState, useEffect, useReducer } = React;
+  const [query, setQuery] = useState("http://localhost:1337/products");
   const [{ data, isLoading, isError }, doFetch] = useDataApi(
-    "http://localhost:1337/api/products",
+    "http://localhost:1337/products",
     {
       data: [],
     }
-  ); 
-
-  // Add item to cart and remove it from stock
+  );
+  console.log(`Rendering Products ${JSON.stringify(data)}`);
+  // Fetch Data
   const addToCart = (e) => {
     let name = e.target.name;
     let item = items.filter((item) => item.name == name);
+    if (item[0].instock == 0) return;
+    item[0].instock =item[0].instock - 1;
+    console.log(`add to Cart ${JSON.stringify(item)}`);
     setCart([...cart, ...item]);
-
-    // Reduce stock
-    let floorStock = [...items];
-    floorStock.forEach(p => p.name === name ? p.instock-- : p.instock = p.instock);
-    setItems([...floorStock]);
-
+    //doFetch(query);
   };
-
-  // Delete item from cart and add it back to stock
   const deleteCartItem = (index) => {
-    let product = cart.filter((item,i) => index === i)[0];
-    let floorStock = [...items];
-    floorStock.forEach(p => p.name == product.name ? p.instock++ : p.instock += 0);
-    setItems([...floorStock]);
-
     let newCart = cart.filter((item, i) => index != i);
+    let target = cart.filter((item, index) => delIndex == index);
+    let newItems = [...items];
     setCart(newCart);
-
+    setItems(newItems);
   };
   const photos = ["apple.png", "orange.png", "beans.png", "cabbage.png"];
 
   let list = items.map((item, index) => {
-    
-    if (item.instock > 0) {
-      return (
-        <li key={index} style={{display:"flex", margin: "10px 0px", alignItems:"left"}}>
-            <Image src={photos[index % 4]} width={60} style={{flexGrow:0,flexShrink:0}}></Image>
-            <div>
-              <Button style={{margin: "5px", minWidth: "150px", maxHeight: "35px"}} variant="warning" size="large" name={item.name} type="submit" onClick={addToCart}>
-                {item.name} : ${item.cost}
-              </Button>
-              <div style={{margin: "0px 5px", textAlign: "center", fontSize: "0.9rem"}}>{item.instock} in stock</div>
-            </div>
+    let n = index + 1049;
+    let url = "https://picsum.photos/id/" + n + "/50/50";
 
-        </li>
-      );
-    }
-
+    return (
+      <li key={index} style={{display:"flex", margin: "10px 0px", alignItems:"left"}}>
+        <Image src={photos[index % 4]} width={60} style={{flexGrow:0,flexShrink:0}}></Image>
+        <div>
+        <Button style={{margin: "5px", minWidth: "150px", maxHeight: "35px"}} variant="warning" size="large" name={item.name} type="submit" onClick={addToCart}>
+        {item.name} : ${item.cost}
+                 </Button>
+                 <div style={{margin: "0px 5px", textAlign: "center", fontSize: "0.9rem"}}>{item.instock} in stock</div>
+        </div>
+      </li>
+    );
   });
-
   let cartList = cart.map((item, index) => {
     return (
       <Card key={index}>
-        <Card.Header id={"card-header-"+index}>
-        <Accordion.Toggle as={Button} variant="link" eventKey={1 + index}>
-            <b>{item.name}</b> (1)
+        <Card.Header>
+          <Accordion.Toggle as={Button} variant="link" eventKey={1 + index}>
+          <b>{item.name}</b> (1)
           </Accordion.Toggle>
         </Card.Header>
-
         <Accordion.Collapse
-          id={"card-body-"+index} eventKey={1 + index}
+          onClick={() => deleteCartItem(index)}
+          eventKey={1 + index}
         >
           <Card.Body>
-            <div style={{display:"flex", alignItems: "center"}}>
+          <div style={{display:"flex", alignItems: "center"}}>
               <div style={{margin:"5px 10px 5px 5px"}}>$ {item.cost} {item.country}</div>
-              <Button variant="info" size="large" onClick={() => deleteCartItem(index)} style={{fontSize: "0.9em"}}>Remove from Cart</Button>
-            </div>
+              <Button onChange={()=> deleteCartItem(index)}>Item Remove</Button>
+              </div>
           </Card.Body>
         </Accordion.Collapse>
-
-        <Card.Body id={"card-body-"+index} style={{display: "none", fontSize:"0.9rem"}}>
-          
-          
-        </Card.Body>
       </Card>
     );
   });
@@ -166,7 +161,7 @@ const Products = () => {
     let final = cart.map((item, index) => {
       return (
         <div key={index} index={index}>
-          {item.name} - 1
+          {item.name}
         </div>
       );
     });
@@ -177,42 +172,23 @@ const Products = () => {
     let costs = cart.map((item) => item.cost);
     const reducer = (accum, current) => accum + current;
     let newTotal = costs.reduce(reducer, 0);
+    console.log(`total updated to ${newTotal}`);
     return newTotal;
   };
-
-  const restockProducts = async (url) => {
-
-    await doFetch(url);
-
-    let floorStock = [...items];
-
-    if (items.length === 0) {
-      data.data.forEach(({ attributes: p }) => {
-        const {name, country, cost, instock} = p;
-        floorStock.push({ name, country, cost: Number(cost), instock: Number(instock) });
-      });
-    }
-    else {
-      data.data.forEach(({attributes: p}) => {
-        let restockedItem = floorStock.filter(item => item.name == p.name)[0]; 
-        restockedItem ? restockedItem.instock += p.instock : restockedItem.instock += 0;
-      });
-    }
-
-    setItems(floorStock);
-
+  // TODO: implement the restockProducts function
+  const restockProducts = (url) => {
+    doFetch(url);
+    let newItems = data.map((item) => {
+      let { name, country, cost, instock } = item;
+      return { name, country, cost, instock };
+    });
+    setItems([...items, ...newItems]);
   };
-
-  useEffect(() => {
-    if (items.length === 0) {
-      restockProducts("http://localhost:1337/api/products");
-    }
-  }, [items]);
 
   return (
     <Container>
       <Row>
-        <Col style={{paddingBottom: "30px"}}>
+      <Col style={{paddingBottom: "30px"}}>
           <h2 style={{padding:"15px 0px 5px 0px", color:"green"}}>Groceries</h2>
           <ul style={{ listStyleType: "none", paddingLeft: "0px" }}>{list}</ul>
         </Col>
@@ -230,17 +206,18 @@ const Products = () => {
         <form
           style={{marginBottom: "0px"}}
           onSubmit={(event) => {
+            restockProducts(`http://localhost:1337/${query}`);
+            console.log(`Restock called on ${query}`);
             event.preventDefault();
-            restockProducts(`${query}`);
           }}
         >
           <input
-            style={{margin:"5px",padding:"5px", width:"250px"}}
+           style={{margin:"5px",padding:"5px", width:"250px"}}
             type="text"
             value={query}
             onChange={(event) => setQuery(event.target.value)}
           />
-          <button style={{margin:"0px 10px"}} type="submit">{items.length === 0 ? 'Stock Products' : 'Restock Products'}</button>
+           <button style={{margin:"0px 10px"}} type="submit">{items.length === 0 ? 'Stock Products' : 'Restock Products'}</button>
         </form>
       </Row>
     </Container>
